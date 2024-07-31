4b465b1ece5b1c71d96d41487d6232a8b1916683
Robbie <robbie331@>
2024-07-31 09:28:33 +0900

    Level1 - 1 양의 정수

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
+        // 양의 정수 2개(0 포함)을 전달 받을 수 있다. 양의 정수는 각각 하나씩 전달 받는다.
+        // 정수 2개를 각각 하나씩 전달 받는 거라 num1, num2의 이름을 붙였다.
+        // Scanner를 샤용하여 입력 받고 적합한 타입의 변수에 저장하라 해서 sc.nerxtInt();를 썼다.
+        // 정수를 전달 받을 수 있다 하여 int를 썼다.
 
         Scanner sc = new Scanner(System.in);
 
