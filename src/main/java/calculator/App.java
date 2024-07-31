0764b6da1b83f2f44a995a7e492662d7e15893cc
Robbie <robbie331@>
2024-07-31 09:49:22 +0900

    Level1 - 2 사칙연산 기호

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -18,7 +18,15 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();
 
-        int result = 0;
+        System.out.print("사칙연산 기호를 입력하세요: "); // 연산 기호는 +, -, *, /
+        // 사칙연산은 하나하나 사용되기 때문에 string(문자열)보다는 char(문자)를 사용해보자.
+
+        char operator = sc.next().charAt(0); //next는 string 타입을 쓰기 때문에 charAt을 붙여준다.
+        // char operator = sc.nextInt().charAt(0);으로 했을 때 숫자 0 부분에서 자꾸 오류가 남.
+        // char operator = sc.nextInt().charAt('0'); char는 문자에 사용되니 '' 붙여봤지만 오류가 남.
+        // nextInt가 아니라 next만 써야 했다. int는 정수.
+
+        System.out.println("operator = " + operator);
 
     }
 }
